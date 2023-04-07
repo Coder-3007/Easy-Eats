d6bbd06e0664d146776ac175fabea876f8ecc3b9
Coder-3007 <coder3007@gmail.com>
2023-04-07 06:54:50 +0500

    adding restaurant item

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
-import React from "react";
-import { View, SafeAreaView, ScrollView } from "react-native";
-import Categories from "./Components/Categories";
-import HeaderTabs from "./Components/HeaderTabs";
-import RestaurantItem from "./Components/RestaurantItem";
-import SearchBar from "./Components/SearchBar";
+import React, { useState } from "react";
 import Home from "./Screens/Home";
 
 export default function App() {
-  return (
-    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-      <View style={{ backgroundColor: "white", padding: 15 }}>
-        <HeaderTabs />
-        <SearchBar />
-      </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Categories />
-        <RestaurantItem />
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <Home />;
 }
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,20 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, SafeAreaView, ScrollView } from "react-native";
+import Categories from "../Components/Categories";
+import HeaderTabs from "../Components/HeaderTabs";
+import RestaurantItems, {
+  localRestaurants,
+} from "../Components/RestaurantItems";
+import SearchBar from "../Components/SearchBar";
 
-const Home = () => {
+export default function Home() {
+  const [restaurantData, setRestaurantDate] = useState(localRestaurants);
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
+    </SafeAreaView>
   );
-};
-
-export default Home;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+}
